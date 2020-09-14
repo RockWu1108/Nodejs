1ee5bfe299ac036f4a85ebe76c6cfab2446dfb7f
andy70294 <andy70294@gmail.com>
2020-09-14 19:30:25 +0800

    show author

---
+++ routes/authors.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var Author = require('../models/author');
+// all author route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+
+    if(req.query.name !=null && req.query.name!==''){
+        //flags = i 表示忽略大小寫
+        searchOptions.name = new RegExp(req.query.name , 'i');
+    }
+    try {
+        const authors = await  Author.find({name : searchOptions.name})
+        console.log(authors);
+        res.render('authors/index' , {
+            authors : authors,
+            searchOptions : req.query
+
+
+        });
+    }
+    catch (e) {
+        res.redirect('/');
+    }
+});
+
+// new Author route
+router.get('/new' , (req , res) =>{
+    res.render('authors/new' , {author: new Author()});
+});
+
+router.post('/' , async (req , res) =>{
+
+    const author = new Author({
+        name : req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+        res.redirect(`authors`);
+    }
+    catch {
+
+            res.render('authors/new',{
+                author : author,
+                errorMessage : 'error creating'
+            })
+
+    }
+
+    // author.save((error , newAuthor) =>{
+    //     if(error){
+    //         res.render('authors/new',{
+    //             author : author,
+    //             errorMessage : 'error creating'
+    //         })
+    //     }
+    //     else{
+    //         res.redirect(`authors`);
+    //
+    //     }
+    // });
+});
+
+module.exports = router;
